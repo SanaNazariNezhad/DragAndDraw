5353618379a097bb143782f49927e9655db5d77d
SanaNazariNezhad <sana.nazari1995@gmail.com>
2020-12-12 22:31:07 +0330

    add SplashActivity to project

--- app/src/main/java/org/maktab/draganddraw/DragAndDrawActivity.java
+++ app/src/main/java/org/maktab/draganddraw/DragAndDrawActivity.java
@@ -1,8 +1,15 @@
 package org.maktab.draganddraw;
 
+import android.content.Context;
+import android.content.Intent;
+
 import androidx.fragment.app.Fragment;
 
 public class DragAndDrawActivity extends SingleFragmentActivity {
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, DragAndDrawActivity.class);
+    }
 
     @Override
     public Fragment createFragment() {
---
+++ app/src/main/java/org/maktab/draganddraw/SplashActivity.java
@@ -0,0 +1,37 @@
+package org.maktab.draganddraw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        LottieAnimationView lottieAnimationView = findViewById(R.id.lottie_success_work);
+        //request from server using retrofit. [play animation]
+        lottieAnimationView.playAnimation();
+
+
+        //get the result in observer livedata.[go to home activity and finish this one]
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(DragAndDrawActivity.newIntent(SplashActivity.this));
+                finish();
+            }
+        }, 6000);
+    }
+
+    @Override
+    public void onBackPressed() {
+    }
+
+}
